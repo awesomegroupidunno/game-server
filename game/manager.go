b43e49ec331798526791cc18ec010558e676e2d1
Dan Mashuda <dmashuda@ycp.edu>
2015-10-04 23:22:36 -0400

    Fixes #31, race condition

--- game/manager.go
+++ game/manager.go
@@ -9,33 +9,28 @@ import (
 	"time"
 )
 
+var managerLock sync.Mutex
+
 type GameManager struct {
 	startTime         time.Time
 	lastTick          time.Time
 	isStarted         bool
 	isPaused          bool
-	stateMutex        *sync.Mutex
-	commandMutex      *sync.Mutex
 	gameState         state.GameState
 	commandsToProcess []*cmd.GameCommand
 	commandFactory    processor.CommandProcessorFactory
 }
 
 func (g *GameManager) Start() {
-	g.stateMutex = &sync.Mutex{}
-	g.commandMutex = &sync.Mutex{}
+	managerLock.Lock()
 
-	g.commandMutex.Lock()
 	g.commandsToProcess = []*cmd.GameCommand{}
-	g.commandMutex.Unlock()
-
-	g.stateMutex.Lock()
 	g.gameState = state.GameState{}
-	g.stateMutex.Unlock()
-
 	g.isStarted = true
 	g.isPaused = false
 	g.startTime = time.Now()
+
+	managerLock.Unlock()
 
 	for g.isStarted && !g.isPaused && !g.gameState.GameOver {
 		g.tick()
@@ -44,37 +39,34 @@ func (g *GameManager) Start() {
 
 }
 func (g *GameManager) Pause() {
-	g.stateMutex.Lock()
+	managerLock.Lock()
 	g.isPaused = true
-	g.stateMutex.Unlock()
+	managerLock.Unlock()
 }
 func (g *GameManager) Resume() {
-	g.stateMutex.Lock()
+	managerLock.Lock()
 	g.isPaused = false
-	g.stateMutex.Unlock()
+	managerLock.Unlock()
 }
 func (g *GameManager) AddCommand(c cmd.GameCommand) {
-	g.commandMutex.Lock()
+	managerLock.Lock()
 	g.commandsToProcess = append(g.commandsToProcess, &c)
-	g.commandMutex.Unlock()
+	managerLock.Unlock()
 }
 func (g *GameManager) TakeState() state.GameState {
-	g.stateMutex.Lock()
+	managerLock.Lock()
 	a := g.gameState.Copy()
-	g.stateMutex.Unlock()
+	managerLock.Unlock()
 	return a
 }
 func (g *GameManager) tick() {
-	g.commandMutex.Lock()
+	managerLock.Lock()
 	commands := g.commandsToProcess
 	g.commandsToProcess = g.commandsToProcess[:0]
-	g.commandMutex.Unlock()
+	managerLock.Unlock()
 
 	if len(commands) > 0 {
 		log.Printf("Ticking with %d commands", len(commands))
 	}
-
-	g.stateMutex.Lock()
-	g.stateMutex.Unlock()
 
 }
