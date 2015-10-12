df1d18c634d2d2317a6d553f466f53148b5c2557
Dan Mashuda <dmashuda@ycp.edu>
2015-10-12 15:53:00 -0400

    code cleanup

--- processor/processor_test.go
+++ processor/processor_test.go
@@ -9,28 +9,26 @@ import (
 
 func TestProcessorFactory(t *testing.T) {
 	Convey("ProcessorFactory", t, func() {
-		a := processor.CommandProcessorFactory{}
+		factory := processor.CommandProcessorFactory{}
 
 		t := cmd.NewTurn(1)
 		t_comm := cmd.GameCommand(&t)
-
-		ac := cmd.NewAcceleration(1)
-		a_comm := cmd.GameCommand(&ac)
-
-		connect := cmd.NewConnect("test")
-		c_comm := cmd.GameCommand(&connect)
-
-		base := cmd.BaseCommand{}
-		b_comm := cmd.GameCommand(&base)
-
-		turn_cp := a.GetCommandProcessor(&t_comm)
-		acceleration_cp := a.GetCommandProcessor(&a_comm)
-		connect_cp := a.GetCommandProcessor(&c_comm)
-		base_cp := a.GetCommandProcessor(&b_comm)
-
+		turn_cp := factory.GetCommandProcessor(&t_comm)
 		So(turn_cp, ShouldHaveSameTypeAs, &processor.TurnCommandProcessor{})
+
+		a := cmd.NewAcceleration(1)
+		a_comm := cmd.GameCommand(&a)
+		acceleration_cp := factory.GetCommandProcessor(&a_comm)
 		So(acceleration_cp, ShouldHaveSameTypeAs, &processor.AccelerationCommandProcessor{})
+
+		c := cmd.NewConnect("test")
+		c_comm := cmd.GameCommand(&c)
+		connect_cp := factory.GetCommandProcessor(&c_comm)
 		So(connect_cp, ShouldHaveSameTypeAs, &processor.ConnectCommandProcessor{})
+
+		b := cmd.BaseCommand{}
+		b_comm := cmd.GameCommand(&b)
+		base_cp := factory.GetCommandProcessor(&b_comm)
 		So(base_cp, ShouldEqual, nil)
 	})
 }
